091fb645f3077de55229c4775867ed3c06a4411e
Guyddo <guyddogl@gmail.com>
2022-11-05 11:58:45 -0300

    Require httpStatus

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const { HTTP_STATUS_OK } = require('../utils/httpStatus');
 const { generateToken } = require('../utils/token');
 const { validateEmail, validatePassword } = require('../middleware/validateLogin');
 
 const router = express.Router();
 
-const HTTP_OK_STATUS = 200;
-
 router.post('/', validateEmail, validatePassword, async (_req, res) => {
-  res.status(HTTP_OK_STATUS).json({ token: generateToken() });
+  res.status(HTTP_STATUS_OK).json({ token: generateToken() });
 });
 
 module.exports = router;
